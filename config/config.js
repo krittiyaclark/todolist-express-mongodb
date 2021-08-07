cc970dc112d47cdf4fa27540a4d9367de4d2d2a0
Krittiya <krittiyaclark@gmail.com>
2021-08-06 20:27:43 -0400

    Styles the home page and the sidebar

--- config/config.js
+++ config/config.js
@@ -2,9 +2,9 @@ exports.creds = {
 	identityMetadata:
 		'https://login.microsoftonline.com/common/v2.0/.well-known/openid-configuration',
 
-	clientID: '<clientID>',
+	clientID: 'f5a669b3-5c83-4983-86c6-e8b6ba43bd3a',
 
-	clientSecret: '<clientSecret>',
+	clientSecret: 'U~8g-a6Rc3.hHO1i89b4jrtPJ1p-Ah~02E',
 
 	responseType: 'code id_token',
 
--- config/dist/config.dev.js
+++ config/dist/config.dev.js
@@ -2,8 +2,8 @@
 
 exports.creds = {
   identityMetadata: 'https://login.microsoftonline.com/common/v2.0/.well-known/openid-configuration',
-  clientID: '<clientID>',
-  clientSecret: '<clientSecret>',
+  clientID: 'f5a669b3-5c83-4983-86c6-e8b6ba43bd3a',
+  clientSecret: 'U~8g-a6Rc3.hHO1i89b4jrtPJ1p-Ah~02E',
   responseType: 'code id_token',
   responseMode: 'form_post',
   redirectUrl: 'http://localhost:2121/auth/openid/return',
